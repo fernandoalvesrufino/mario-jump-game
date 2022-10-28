55eb6fa8c409e4c3b4e800ed54acb58dad994012
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-10-28 08:41:59 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -19,11 +19,25 @@ let loop = setInterval(function () {
 
   const pipePosition = parseInt(window.getComputedStyle(pipe).getPropertyValue("left"));
   const marioPosition = parseInt(window.getComputedStyle(mario).getPropertyValue("bottom"));
+  /* 
+  - pipePosition e marioPosition recebem a posicao atual do elemento
+  - window.getComputedStyle() retorna um objeto contendo os valores de todas as propriedades CSS de um elemento
+  - getPropertyValue() retorna uma string contendo o valor de uma propriedade CSS especificada
+  - getComputedStyle().getPropertyValue() retorna uma string contendo o valor de uma determinada propriedade do CSS
+  - parseInt() analisa um argumento string e retorna um inteiro
+  */
   pontuacaoAtual = pontuacaoAtual + 0.09;
+  //aumenta a pontuacao enquanto o jogo ainda esta rodando
+  
   const info = "Pontuação: " + pontuacaoAtual.toFixed(0); 
+  //toFixed() formata um número. Se 0 (zero) formata para numero sem casas decimais (numero inteiro)
+  
   pontuacao.textContent = info
+  //adiciona a nova string 
+  
 
   if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+  //Se esses parametros forem atendidos significa que o 'mario' e 'pipe' colidiram
 
     pipe.style.animation = "none";
     pipe.style.left = `${pipePosition}px`;
